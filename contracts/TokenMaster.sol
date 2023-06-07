7c28e5d6f39cd540b3b03e504af5b9e58c382fbe
workwithamit <workwithamit89@gmail.com>
2023-06-07 19:13:24 +0530

    initialized repo

---
+++ contracts/TokenMaster.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract TokenMaster is ERC721 {
+    address public owner;
+    uint256 public totalOccasions;
+    uint256 public totalSupply;
+
+    struct Occasion {
+        uint256 id;
+        string name;
+        uint256 cost;
+        uint256 tickets;
+        uint256 maxTickets;
+        string date;
+        string time;
+        string location;
+    }
+}
