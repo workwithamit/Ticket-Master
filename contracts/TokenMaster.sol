310582050f62a028c0fbd99a5e175ed3349ef6f7
workwithamit <workwithamit89@gmail.com>
2023-06-10 00:07:31 +0530

    chainlink integration

--- contracts/TokenMaster.sol
+++ contracts/TokenMaster.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract TokenMaster is ERC721 {
     address public owner;
